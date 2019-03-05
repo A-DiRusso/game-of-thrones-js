05a6ea872001863ed7c69b5c463f5cb202f11317
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 17:45:50 -0500

    adds busted-ish died count

--- scripts/index.js
+++ scripts/index.js
@@ -9,5 +9,37 @@ console.log(aPeeps);
 // characters with "Z" names
 let zPeeps = characters.filter(char => {
     return char.name[0] === "Z";
-})
+});
 console.log(zPeeps);
+
+
+
+
+
+function bringOutYourDead(characters, item) {
+    count = 2124;
+    let allTheInfo = Object.keys(characters);
+    allTheInfo.forEach(function(findDead) {
+        let dead = characters[findDead][item];
+        if (dead == false) {
+            count -= 1;
+        }
+        console.log(count);
+        // filter(dead);
+        //console.log(dead);
+    });
+}
+
+bringOutYourDead(characters, 'died');
+
+// function filter(dead) {
+//     let deadCount = 0;
+//     if (dead !== " ") {
+//         deadCount += 1;
+//     }
+//     deadIsCount(deadCount);
+// }
+
+// function deadIsCount(deadCount) {
+//     console.log(deadCount);
+// }
