a7dfbcd94459e780d8a6cb57eafe38256846d5a3
Anthony DiRusso <ossurid@gmail.com>
2019-03-05 16:35:08 -0500

    adds 'Z' names filter

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,13 @@
 console.log(`There are ${characters.length} characters in the array.`);
 
+// characters with "A" names
 let aPeeps = characters.filter(char => {
     return char.name[0] === "A"
 });
 console.log(aPeeps);
 
+// characters with "Z" names
+let zPeeps = characters.filter(char => {
+    return char.name[0] === "Z";
+})
+console.log(zPeeps);
